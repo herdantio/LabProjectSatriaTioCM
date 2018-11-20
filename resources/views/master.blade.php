48098a0a098bc494447cc46805ad8b5a1a036f6d
herdantio <herdantio.sindhunata@binus.ac.id>
2018-11-21 04:09:09 +0700

    navbar link

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,7 +23,7 @@
     </div> --}}
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="/">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -32,10 +32,10 @@
         
         @if(Auth::guest())
             <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" href="/login">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
+                <a class="nav-link" href="/register">Register</a>
             </li>
         @else
             <li class="nav-item">
