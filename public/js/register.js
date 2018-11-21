3babf1ce47a932ec662dda2041247f41376ebf8d
herdantio <herdantio.sindhunata@binus.ac.id>
2018-11-21 12:45:08 +0700

    update register js

--- public/js/register.js
+++ public/js/register.js
@@ -1,9 +1,10 @@
-let password = document.getElementById("passwordOriginal").value
-let password2 = document.getElementById("passwordReType").value
-let registerBtn = document.getElementById("registerBtn")
-
 
 function validate(){
+
+    let password = document.getElementById("passwordOriginal").value
+    let password2 = document.getElementById("passwordReType").value
+    let registerBtn = document.getElementById("registerBtn")
+
     if(password!=password2){
         alert("Password must be the same")
         event.preventDefault()
