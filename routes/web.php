97fc011b77708cfce1ea412fc71ecb45903e677c
SatriaHR <satria.hanafi5@gmail.com>
2018-11-22 11:30:47 +0700

    tidying code

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+//halaman paling pertama
 Route::get('/', function () {
     return view('home');
 });
@@ -18,15 +19,14 @@ Route::get('/', function () {
 //Route::get('/login', function () {
 //    return view('login');
 //});
-Route::post('/login', 'UserController@login');
-Route::get('/login', 'UserController@login_get');
-
 
 //Route::get('/register', function () {
 //    return view('register');
 //});
 Route::post('/register', 'UserController@register');
 Route::get('/register', 'UserController@register_get');
+Route::post('/login', 'UserController@login');
+Route::get('/login', 'UserController@login_get');
 Route::get('/logout', 'UserController@logout');
 
 Route::get('/manageUsers', 'UserController@manageUsers');
