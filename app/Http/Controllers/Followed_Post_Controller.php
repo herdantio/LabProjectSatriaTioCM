3ec83c5c553939143ecde723f5e8012be82973d6
SatriaHR <satria.hanafi5@gmail.com>
2018-12-01 11:15:09 +0700

    Update Followed_Post_Controller.php

--- app/Http/Controllers/Followed_Post_Controller.php
+++ app/Http/Controllers/Followed_Post_Controller.php
@@ -11,12 +11,13 @@ class Followed_Post_Controller extends Controller
     public function followedposts_getPage(){
         $user_id = Auth::user()->id;
 
-        $fp = Followed_Post::where('follower_id', 'EQUALS', $user_id);
+        //$fp = Followed_Post::where('follower_id', 'EQUALS', $user_id);
+        $fp = Followed_Post::where('follower_id', '=', $user_id);
 
         //get all posts
         $posts = null;
         foreach($fp as $f){
-            $posts += Followed_Post::where('post_id', 'EQUALS', $f->post_id);
+            $posts += Followed_Post::where('post_id', '=', $f->post_id);
         }
         $posts->paginate(10);
 
