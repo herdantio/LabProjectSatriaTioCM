5b672f67ab3f2b29ae63c26296d6215205fdebd0
herdantio <herdantio.sindhunata@binus.ac.id>
2018-12-01 11:45:25 +0700

    update watch

--- public/js/navbar-watch.js
+++ public/js/navbar-watch.js
@@ -2,7 +2,7 @@ var myVar = setInterval(myTimer, 1000);
 
 function myTimer() {
     var d = new Date();
-    // var t = d.toLocaleTimeString();
-    var t = d.toLocaleString('en-GB')
+    var t = d.toLocaleTimeString('it-IT');
+    // var t = d.toLocaleString('en-GB')
     document.getElementById("navbar-clock").innerHTML = t;
 }
