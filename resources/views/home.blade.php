76ba0452379cd284473716a52de5c955696361f1
herdantio <herdantio.sindhunata@binus.ac.id>
2018-12-18 16:55:53 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('content')
-    <link href = {{ asset("css/register.css") }} rel="stylesheet"/>
+    <link href = {{ asset("css/home.css") }} rel="stylesheet"/>
     <div class="container">
         <div class="row">
             @if(!Auth::guest())
@@ -13,14 +13,23 @@
 
             <div class="container">
                 <div class="row">
-                    @foreach ($p as $post)
-                        <div class="col-md-2" >
-                            <img src="{{asset("UsersUploadedImage/").'/'.$post->picture}}"/>
+                    @foreach ($p as $index => $post)
+                        <div class="col-md-2" class="post">
+                            <img class="img-post" src="{{asset("UsersUploadedImage/").'/'.$post->picture}}"/>
                             <h3>{{$post->title}}</h3>
                             <p>{{$post->caption}}</p>
                         </div>
                     @endforeach
+                    {{--@for($i=0;$i<5;$i++)--}}
+                        {{--<div class="col" class="post">--}}
+                            {{--<img class="img-post" src="{{asset("UsersUploadedImage/").'/'.$p[$i]['picture']}}"/>--}}
+                            {{--<h3>{{$p[$i]['title']}}</h3>--}}
+                            {{--<p>{{$p[$i]['caption']}}</p>--}}
+                        {{--</div>--}}
+                    {{--@endfor--}}
+
                 </div>
+
             </div>
 
         </div>
