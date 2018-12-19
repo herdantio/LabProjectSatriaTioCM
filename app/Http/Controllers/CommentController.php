b66789763440ff4106f579b2fb354ca05404bcda
SatriaHR <satria.hanafi5@gmail.com>
2018-12-19 14:16:42 +0700

    fix sql showComment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -24,7 +24,7 @@ class CommentController extends Controller
 
         //if already followed
         $user_id = Auth::user()->id;
-        $fpost = Followed_Post::where(['post_id', '=', $post_id], ['follower_id', '=', $user_id])->first();
+        $fpost = Followed_Post::where([['post_id', '=', $post_id], ['follower_id', '=', $user_id]])->first();
         $followed = true;
         if($fpost == null){
             $followed = false;
